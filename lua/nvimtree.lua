891958bf2a5eff8207bbc5315b81230baf217530
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2023-02-01 23:13:47 +0100

    colors customized

--- lua/nvimtree.lua
+++ lua/nvimtree.lua
@@ -2,21 +2,21 @@
 local make = function(c)
     return {
         -- LuaFormatter off
-        {hlgroup = 'NvimTreeNormal', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeVertSplit', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeRootFolder', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeGitDirty', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeGitNew', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeImageFile', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeFolderIcon', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeIndentMarker', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeEmptyFolderName', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeFolderName', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeSpecialFile', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeOpenedFolderName', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeCursorLine', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeIn', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
-        {hlgroup = 'NvimTreeEndOfBuffer', guifg = c.base08, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeNormal', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeVertSplit', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeRootFolder', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeGitDirty', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeGitNew', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeImageFile', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeFolderIcon', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeIndentMarker', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeEmptyFolderName', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeFolderName', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeSpecialFile', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeOpenedFolderName', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeCursorLine', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeIn', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
+        {hlgroup = 'NvimTreeEndOfBuffer', guifg = nil, guibg = c.base01, gui = 'none', guisp = nil},
         -- LuaFormatter on
     }
 end
