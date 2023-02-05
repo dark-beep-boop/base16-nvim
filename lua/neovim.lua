8ec98ffee492abf9c668bc3217338c91c25bc1d7
Jesús María Gómez Moreno <je.ov.999@gmail.com>
2023-02-05 01:28:06 +0100

    border background changed

--- lua/neovim.lua
+++ lua/neovim.lua
@@ -4,7 +4,7 @@ local make = function(c)
         -- LuaFormatter off
         {hlgroup = 'NvimInternalError', guifg = c.base00, guibg = c.base08, gui = 'none', guisp = nil},
         {hlgroup = 'NormalFloat', guifg = c.base05, guibg = c.base01, gui = nil, guisp = nil},
-        {hlgroup = 'FloatBorder', guifg = c.base05, guibg = c.base01, gui = nil, guisp = nil},
+        {hlgroup = 'FloatBorder', guifg = c.base05, guibg = c.base00, gui = nil, guisp = nil},
         {hlgroup = 'NormalNC', guifg = c.base05, guibg = c.base00, gui = nil, guisp = nil},
         {hlgroup = 'TermCursor', guifg = c.base00, guibg = c.base05, gui = 'none', guisp = nil},
         {hlgroup = 'TermCursorNC', guifg = c.base00, guibg = c.base05, gui = nil, guisp = nil}
